e9b9df3ca1a42e2f490d66f28b639fa5aeae3453
miquel <miqmar06@gmail.com>
2026-03-24 13:31:24 +0100

    Cambios hechos
    El mismo commit de antes solo que no había hecho ctrl+s

--- backend/settAPI/Classes/WorkSession.cs
+++ backend/settAPI/Classes/WorkSession.cs
@@ -1,6 +1,11 @@
 ﻿namespace settAPI.Classes
 {
+    // SEGUIR AQUÍ, https://claude.ai/share/821499d1-f429-4cc5-b1f0-103654bf7824 (LINK CLAUDE CONVERSACIÓN)
     public class WorkSession
     {
+        public int worker_id { get; set; }
+        public DateTime started_at { get; set; }
+        public DateTime ended_at { get; set; }
+        public int total_minutes { get; set; }
     }
 }
