b9e0d08d2f01a9e3f3e188b0ddc127b9082e470a
DieegowCC <diegoalejandrocardonacastillo@gmail.com>
2026-03-27 13:29:07 +0100

    Frontend Development
    First version of our dashboard using shadcn and tailwindcss

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,65 +1,85 @@
-import Image from "next/image";
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { Button } from "@/components/ui/button"
+import { DataTable } from "@/components/data-table"
+import { columns, Payment } from "@/components/columns"
+import data from "@/app/data.json";
+
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <SidebarProvider>
+    <AppSidebar />
+    <SidebarInset>
+      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+        <SidebarTrigger className="-ml-1" />
+        <Separator
+          orientation="vertical"
+          className="mr-2 data-[orientation=vertical]:h-4"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbLink href="#">Sett</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator className="hidden md:block" />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Admin's Dashboard</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </header>
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        <div className="grid auto-rows-min md:grid-cols-4 gap-10 md:gap-2 m-4">
+          <div className="aspect-video rounded-xl col-start-3 row-start-1 md:col-start-4 md:row-start-1 md:col-span-1 md:row-span-1 bg-blue-400 p-10">
+          <Button>Login</Button>
+          <p>Avatar</p>
+          </div>
+          <div className="aspect-video rounded-xl col-start-2 row-start-2 md:col-start-2 md:row-start-2 md:col-span-1 md:row-span-1 bg-amber-300 p-10" />
+          <div className="aspect-video rounded-xl col-start-3 row-start-2 md:col-start-3 md:row-start-2 md:col-span-1 md:row-span-1 bg-green-300" />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+        {/* <DataTable>columns={columns} data={data}</DataTable> */}
         </div>
-      </main>
-    </div>
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
   );
 }
+
+/* <div className="grid grid-cols-3 md:grid-cols-4 grid-rows-5 md:grid-rows-5 gap-10 md:gap-2 m-4">
+<div className="col-start-1 row-start-1 row-span-5 md:col-start-1 md:row-start-1 md:col-span-1 md:row-span-5 bg-mist-600 rounded-md p-10">
+  <h1>Sett</h1>
+  <ul>
+    <li>Inicio</li>
+    <li>Herramientas</li>
+    <li>Gestion de ordenadores</li>
+    <li>Filtrar</li>
+    <li>Asistencia</li>
+  </ul>
+</div>
+<div className="col-start-3 row-start-1 md:col-start-4 md:row-start-1 md:col-span-1 md:row-span-1  bg-blue-400 rounded-md p-10">
+  <p>Login</p>
+  <p>Avatar</p>
+</div>
+<div className="col-start-2 row-start-2 md:col-start-2 md:row-start-2 md:col-span-1 md:row-span-1 bg-amber-300 rounded-md p-10">Usuarios activos</div>
+<div className="col-start-3 row-start-2 md:col-start-3 md:row-start-2 md:col-span-1 md:row-span-1 bg-green-300 rounded-md p-10">Inactividad</div>
+<div className="col-start-2 row-start-4 col-span-2 row-span-2 md:col-start-2 md:row-start-4 md:col-span-2 md:row-span-2  bg-mist-600 rounded-md p-10">Trabajadores activos</div>
+<div className="hidden md:block md:col-start-4 md:row-start-2 md:col-span-1 md:row-span-1 bg-blue-600 rounded-md p-10">Pantallas</div>
+<div className="hidden md:block md:col-start-4 md:row-start-4 md:col-span-1 md:row-span-1  bg-mist-600 rounded-md p-10">Actividad en tiempo real</div>
+<div className="hidden md:block md:col-start-4 md:row-start-5 md:col-span-1 md:row-span-1  bg-mist-600 rounded-md p-10">Apps más usadas</div>
+</div> */
---
+++ frontend/components/app-sidebar.tsx
@@ -0,0 +1,162 @@
+"use client"
+
+import * as React from "react"
+
+import { SearchForm } from "@/components/search-form"
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+import { PlusIcon, MinusIcon, House } from "lucide-react"
+
+// This is sample data.
+const data = {
+  navMain: [
+    {
+      title: "Home",
+      url: "#",
+      items: [
+        {
+          title: "Installation",
+          url: "#",
+        },
+        {
+          title: "Project Structure",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Settings",
+      url: "#",
+      items: [
+        {
+          title: "General",
+          url: "#",
+        },
+        {
+          title: "Profile Picture",
+          url: "#",
+          isActive: true,
+        },
+        {
+          title: "Security",
+          url: "#",
+        },
+        {
+          title: "Account",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "API Reference",
+      url: "#",
+      items: [
+        {
+          title: "Components",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Architecture",
+      url: "#",
+      items: [
+        {
+          title: "Accessibility",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Community",
+      url: "#",
+      items: [
+        {
+          title: "Contribution Guide",
+          url: "#",
+        },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <House   className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-medium">Sett</span>
+                  <span className="">v0.0.1</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        <SearchForm />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {data.navMain.map((item, index) => (
+              <Collapsible
+                key={item.title}
+                defaultOpen={index === 1}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      {item.title}{" "}
+                      <PlusIcon className="ml-auto group-data-[state=open]/collapsible:hidden" />
+                      <MinusIcon className="ml-auto group-data-[state=closed]/collapsible:hidden" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  {item.items?.length ? (
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.items.map((item) => (
+                          <SidebarMenuSubItem key={item.title}>
+                            <SidebarMenuSubButton
+                              asChild
+                              isActive={item.isActive}
+                            >
+                              <a href={item.url}>{item.title}</a>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  ) : null}
+                </SidebarMenuItem>
+              </Collapsible>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
