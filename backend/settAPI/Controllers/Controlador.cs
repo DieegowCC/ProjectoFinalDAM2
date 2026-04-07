c6bac9f7c27f20e8e75b3bfbe5a10b9fe619a769
Miquel <miqmar06@gmail.com>
2026-04-07 13:22:32 +0200

    Cambios 11:30 - 13:30 | 07/04/2026
    - Api creada y configurada (para un solo caso, crear usuarios admin manualmente) para que desde los ordenadores del instituto se pueda acceder y postear al Postgres del servidor alojado en el pc de Alejandro (el de clase).
    - Settings cambiados (los dos tipos).

--- backend/settAPI/Controllers/Controlador.cs
+++ backend/settAPI/Controllers/Controlador.cs
@@ -1,16 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using settAPI.Classes;
+using settAPI.Data;
 
 namespace settAPI.Controllers;
 
-// Para los controllers mirarse esto https://learn.microsoft.com/es-es/aspnet/core/web-api/?view=aspnetcore-10.0 
-
-// Aqui van a ir todos los endpoints a los que se puede entrar
 [ApiController]
-[Route("[]")]
+[Route("api/[controller]")]
+public class AdminController : ControllerBase
+{
+    private readonly AppDbContext _context;
 
-    public class Controlador
+    public AdminController(AppDbContext context)
     {
-        
+        _context = context;
     }
 
-public ActionResult<> 
+    // POST: api/admin
+    [HttpPost]
+    public async Task<ActionResult> CreateAdmin([FromBody] Admin admin)
+    {
+        try
+        {
+            admin.created_at = DateTime.UtcNow;
+
+            await _context.Admins.AddAsync(admin);
+            await _context.SaveChangesAsync(); 
+
+            return Ok(new
+            {
+                mensaje = "Admin creado correctamente",
+                admin
+            });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new
+            {
+                error = "Error al guardar en la base de datos",
+                detalle = ex.Message
+            });
+        }
+    }
+
+    // GET: api/admin
+    [HttpGet]
+    public async Task<ActionResult> GetAdmins()
+    {
+        var admins = await _context.Admins.ToListAsync();
+        return Ok(admins);
+    }
+}
